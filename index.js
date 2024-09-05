e4ce04d0e8bf84c77a6adcc0bb6c53ac0b2f744b
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-05 12:49:02 +0300

    another one

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
 
-const users = [
-  { username: 'user1', password: 'password1', role: 'admin' },
-  { username: 'user2', password: 'password2', role: 'user' },
-  { username: 'user3', password: 'password3', role: 'moderator' },
-];
-
-console.log("dsfdsf");
-
-app.use(bodyParser.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: '*',
-  }),
-);
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  const user = users.find((u) => u.username === username && u.password === password);
-
-  if (user) {
-    res.status(200).json({ message: 'Login successful', user });
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' });
-  }
+app.get('/', (req, res) => {
+  res.send('Express JS on Vercel');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get('/ping', (req, res) => {
+  res.send('pong 🏓');
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err, res) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log('[INFO] Server Running on port:', port);
+  }
 });
